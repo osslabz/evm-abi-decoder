e3202e5afc36f724a325d521d2c086103463690a
Raphael Vullriede <raphael@osslabz.net>
2024-02-29 20:29:48 +0100

    switch back to Java 8 to ensure Android compatbility

--- src/main/java/net/osslabz/evm/abi/decoder/AbiDecoder.java
+++ src/main/java/net/osslabz/evm/abi/decoder/AbiDecoder.java
@@ -5,8 +5,9 @@ import org.bouncycastle.util.encoders.Hex;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -20,7 +21,7 @@ public class AbiDecoder {
     Map<String, AbiDefinition.Entry> methodSignatures = new HashMap<>();
 
     public AbiDecoder(String abiFilePath) throws IOException {
-        this.abi = AbiDefinition.fromJson(Files.readString(Path.of(abiFilePath)));
+        this.abi = AbiDefinition.fromJson(new String(Files.readAllBytes(Paths.get(abiFilePath)), StandardCharsets.UTF_8));
         init();
     }
 
